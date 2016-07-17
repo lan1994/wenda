e27e77c15bbedb6e202a8ef406ae582c6c22a1bd
lan1994 <774745012@qq.com>
2016-07-17 18:57:47 +0800

    p

--- wenda/src/wenda/Test.java
+++ wenda/src/wenda/Test.java
@@ -3,7 +3,7 @@ package wenda;
 public class Test {
 public static void main(String[] args) {
 	System.out.println("test git");
-	System.out.println();
+	System.out.println("k");
 	System.out.println();
 }
 }
